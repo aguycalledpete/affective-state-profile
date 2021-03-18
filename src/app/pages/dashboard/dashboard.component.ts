83cd47d4808ad601ad6e0e669951394c9e2b73db
Peter McCullagh <petermccullagh9@gmail.com>
2021-03-18 20:49:41 +0000

    Dashboard updated

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { faChartBar, faPlus, faTable } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  faChartBar = faChartBar;
+  faTable = faTable;
+  faPlus = faPlus;
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
 }
